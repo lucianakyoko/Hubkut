8a0ae4575c8768e84041de365ec4fd59c58f4e8b
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-06-07 15:56:18 -0300

    style(styles): create colors.js

---
+++ src/styles/colors.js
@@ -0,0 +1,21 @@
+export const colors = {
+  primary600: '#5579A1',
+  primary500: '#2E7BB4',
+  primary400: '#308BC5',
+  primary300: '#6F92BB',
+  primary200: '#BBCDE8',
+  primary100: '#D9E6F6',
+
+  secondary900: '#000000',
+  secondary800: '#333333',
+  secondary700: '#5A5A5A',
+  secondary600: '#999999',
+  secondary500: '#C5C6CA',
+  secondary400: '#ECF2FA',
+  secondary300: '#F4F4F4',
+  secondary200: '#F1F9FE',
+  secondary100: '#FFFFFF',
+
+  highlights300: '#D81D99',
+  highlights300: '#ED2590'
+};
