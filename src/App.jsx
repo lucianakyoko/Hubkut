0b11442af8dbb9efc7e3a7a0e940acf0315554d9
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-06-07 17:33:09 -0300

    feat(App): add Home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
