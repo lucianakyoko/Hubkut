64d33feca516f5404f2ea21c52a03170a6fba9c9
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-06-08 20:44:54 -0300

    feat(pages/Home/Login): add user login funcionality using API

--- src/pages/Home/Login/index.jsx
+++ src/pages/Home/Login/index.jsx
@@ -1,3 +1,8 @@
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import client from '../../../services/client';
+import { context } from '../../../context';
+
 import developer from '../../../assets/images/developer.svg';
 import {
   LoginWrapper,
@@ -6,6 +11,22 @@ import {
 } from './style';
 
 export function Login() {
+  const nav = useNavigate();
+  const ctx = useContext(context);
+  const [loginValue, setLoginValue] = useState('');
+
+  const getUserData = event => {
+    event.preventDefault();
+
+    client.get(`/${loginValue}`)
+      .then(response => {
+        ctx.setUserData(response.data);
+        nav('user');
+        console.log(response.data)
+      })
+      .catch(err => console.log(err))
+  }
+
   return(
     <LoginWrapper>
       <img src={developer} alt="ilustração de um humano com computador" />
@@ -16,8 +37,10 @@ export function Login() {
           <input 
             type="text"
             placeholder='Digite seu usuário'
+            value={loginValue}
+            onChange={event => setLoginValue(event.target.value)}
           />
-          <button>Login</button>
+          <button onClick={getUserData}>Login</button>
         </Form>
       </FormWrapper>
     </LoginWrapper>
