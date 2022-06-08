658db17b46734176c65e4f89bd3a498fba047110
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-06-08 20:41:30 -0300

    refactor(App): change route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { ContextProvider } from './context';
 import { Home } from './pages/Home';
 import { User } from './pages/User';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<User/>} />
-      </Routes>
-    </BrowserRouter>
+    <ContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='user' element={<User/>} />
+        </Routes>
+      </BrowserRouter>
+    </ContextProvider>
   )
 }
 
