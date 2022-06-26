e71a8c64b98c63966f875567f14a6b63de8a81f9
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-06-26 15:21:07 -0300

    feat(context): add followers and followins list state

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,14 +1,19 @@
 import React, { useState, createContext} from "react";
-
 export const context = createContext();
 
 export const ContextProvider = props => {
-  const [userData, setUserData] = useState({})
+  const [userData, setUserData] = useState({});
+  const [followersList, setFollowersList] = useState([]);
+  const [followingList, setFollowingList] = useState([]);
 
   return (
     <context.Provider value={{
       userData,
-      setUserData
+      setUserData,
+      followersList,
+      setFollowersList,
+      followingList,
+      setFollowingList
     }}>
       {props.children}
     </context.Provider>
